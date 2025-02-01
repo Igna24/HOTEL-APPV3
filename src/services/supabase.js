a3ae5a31fab0c0df99e595628de7a1fac84ccc42
Ignacio Marin <ignaciomaringarro@gmail.com>
2025-01-31 21:47:38 -0600

    change supabase directions urls

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://svabkiiddnnzikvsewcu.supabase.co";
+export const supabaseUrl = "https://ngsogatmhhixklinalxh.supabase.co";
 const supabaseKey =
-  "eeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nc29nYXRtaGhpeGtsaW5hbHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzNzA2NTAsImV4cCI6MjA1Mzk0NjY1MH0.wE_a0VWC8ZiNSPJvJuYatWPDhwASjOOv65rKicgCP_U";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nc29nYXRtaGhpeGtsaW5hbHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzNzA2NTAsImV4cCI6MjA1Mzk0NjY1MH0.wE_a0VWC8ZiNSPJvJuYatWPDhwASjOOv65rKicgCP_U";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
